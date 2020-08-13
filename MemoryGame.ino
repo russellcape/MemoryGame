85ed27e1e5947a67bef07de2d8501a36f26e04dc
Russell <russellcape98@gmail.com>
2020-08-13 01:28:17 -0400

    Created buzzer variable and tones[] for UNO R3 and Created user defined variables

--- MemoryGame.ino
+++ MemoryGame.ino
@@ -20,3 +20,9 @@ const in led1 = 7; /*Yellow LED.*/
 const in led1 = 8; /*Green LED.*/
 const in led1 = 9; /*Red LED.*/
 const in led1 = 10; /*Blue LED.*/
+
+const int buzzer = 12;
+
+const int tones[] = {1900, 1600, 1300, 1000, 3200};
+
+int buttonState[] = {0,0,0,0};
